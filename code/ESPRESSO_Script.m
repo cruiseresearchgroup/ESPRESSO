3463a9fcb8a4cc86151a0cfe0277a4457899be5c
Shohreh <deldari.shohreh@gmail.com>
2022-07-10 21:08:41 +0100

    The evalutation part is now separated

--- code/ESPRESSO_Script.m
+++ code/ESPRESSO_Script.m
@@ -1,8 +1,8 @@
 % run ESPRESSO
-function ESPRESSO(GT, data, subsequence, chain_len)
+function ESPRESSO(K, data, subsequence, chain_len)
 %%% Input 
 % data : input original data ,
-% GT : segment boundaries, 
+% K : number of segments, 
 % subsequence : window size
 % chain_len : maximum length of chain for each subsequence
 %%% returns:
@@ -17,15 +17,17 @@ function ESPRESSO(GT, data, subsequence, chain_len)
     for i = [1:size(MP,1)]
         [~, wcac(i,:)] = calculateSemanticDensityMatrix(MP,MPI, chain_length, subsequence);
     end
-    [espTT,~] = separateGreedyIG(data, length(GT_TT)+1, wcac, 0.01);
-    [P,R,rmse1,rmse2,F] = calculateScore(GT_TT, espTT, lenTS, margin);
-    [mae,~] = calcScoreMAE(GT_TT, espTT, lenTS);
-    
-    return espTT, F, mae
-
+    [espTT,~] = separateGreedyIG(data, K, wcac, 0.01);
+    return espTT
 
 function [MP, MPI] = computMP(Integ_TS, subsequence)
     for i=1:1:size(Integ_TS,1)
         [MP(i,:), MPI(i,:)] = timeseriesSelfJoinFast(Integ_TS(i,:),subsequence);
     end
 end
+
+
+%% To calculate the score use the following 
+% 
+%    [Precision,Recal,rmse1,rmse2,Fscore] = calculateScore(GT_TT, espTT, lenTS, margin);
+%    [mae,~] = calcScoreMAE(GT_TT, espTT, lenTS);
